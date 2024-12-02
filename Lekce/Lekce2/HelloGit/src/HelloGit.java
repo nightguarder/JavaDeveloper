185f2c02e6abf11c1267107d2610b027b1419053
nightguarder <73370044+nightguarder@users.noreply.github.com>
2024-12-02 14:01:53 +0100

    Add lekce 2 and gitignore

---
+++ Lekce/Lekce2/HelloGit/src/HelloGit.java
@@ -0,0 +1,11 @@
+public class HelloGit {
+    //Main 
+    public static void main(String[] args) throws Exception {
+        System.out.println(hello()); //Print the string at new line
+    }
+
+    //Method hello
+    public static String hello() {
+        return "Hello Git!"; //Return the string
+    }
+}
