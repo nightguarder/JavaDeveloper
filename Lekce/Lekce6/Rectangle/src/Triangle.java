56e595f4f0b38b9c3659b408592900475347da2d
nightguarder <73370044+nightguarder@users.noreply.github.com>
2025-01-14 20:30:54 +0100

    better code

--- Lekce/Lekce6/Rectangle/src/Triangle.java
+++ Lekce/Lekce6/Rectangle/src/Triangle.java
@@ -26,10 +26,6 @@ public class Triangle {
             System.out.println("Invalid input. Size must be greater than 0.");
             return;
         }
-        else if (size >= Integer.MAX_VALUE) {
-            System.out.println("Invalid input. Size must be less than INT_MAX.");
-            return;
-        }
         else {
             for (int i = 1; i <= size; i++) {
                 for (int j = 1; j <= size; j++) { //leading spaces
@@ -47,25 +43,19 @@ public class Triangle {
             System.out.println("Invalid input. Size must be greater than 0.");
             return;
         }
-        else if (size >= Integer.MAX_VALUE) {
-            System.out.println("Invalid input. Size must be less than INT_MAX.");
-            return;
-        }
         else {
             for (int i = 1; i <= size; i++) {
                 for (int j = 1; j <= size; j++) { 
-                if (j<= size - i) { //left part of the triangle
-
-                    System.out.print(' ');
+                    if (j<= size - i) { //left part of the triangle
+                        System.out.print(' ');
+                    }
+                    else {
+                        System.out.print('*');
+                    }
                 }
-                else {
-                    System.out.print('*');
-                }
-            }
             System.out.print('\n');
+            }
         }
-        }
-
     }
 
     public static void main(String[] args) {
