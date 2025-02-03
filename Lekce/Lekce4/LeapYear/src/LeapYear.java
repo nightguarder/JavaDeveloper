587411e18b6c9f86a12222d140ac125c89758181
nightguarder <73370044+nightguarder@users.noreply.github.com>
2025-02-03 21:49:18 +0100

    Leap year done

---
+++ Lekce/Lekce4/LeapYear/src/LeapYear.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public  boolean isLeapYear(int year) {
+        // // Leap year logic:
+        // 1. Divisible by 400, OR
+        // 2. Divisible by 4 but NOT divisible by 100
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Rok "+year +" je přestupný");
+            return true;
+        }
+        else {
+            System.out.println("Rok "+year +" neni přestupný");
+            return false;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Program pro zjištění, zda je zadaný rok přestupný");
+        System.out.println("Zadejte rok: ");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        sc.close(); // zavři scanner
+
+        LeapYear leapYear = new LeapYear();
+        leapYear.isLeapYear(year); // zavolej metoduz pro kontrolu
+        //2023
+        //Rok 2023 neni přestupný
+        //2000
+        //Rok 2000 je přestupný
+    }
+}
