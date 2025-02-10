89f931b1eeb21bdfeb46e94a97693e5b2980ef46
nightguarder <73370044+nightguarder@users.noreply.github.com>
2025-02-10 17:52:03 +0100

    Lekce 18 Multithreading

---
+++ Lekce/Lekce18/MultiThreading/src/Interrupt.java
@@ -0,0 +1,6 @@
+//create an example to stop the thread using the interrupt method
+// There could be athread running connection to server or some API, and we want to stop the thread when the request limit is reached
+//This is often handled by frameworks (Apache, Springboot), but it is good to know how to do it manually
+public class Interrupt {
+    
+}
