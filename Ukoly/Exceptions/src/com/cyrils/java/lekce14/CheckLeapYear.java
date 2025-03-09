a08d832d482fe8ebaeda18e42ff8fea0d39d49bb
nightguarder <73370044+nightguarder@users.noreply.github.com>
2025-03-09 19:28:33 +0100

    implement InvalidYearException

--- Ukoly/Exceptions/src/com/cyrils/java/lekce14/CheckLeapYear.java
+++ Ukoly/Exceptions/src/com/cyrils/java/lekce14/CheckLeapYear.java
@@ -3,16 +3,12 @@ package com.cyrils.java.lekce14;
 import com.cyrils.java.lekce4.LYImproved;
 
 public class CheckLeapYear {
-    public static void main(String[] args) {
-        int year = 2000;
-        // Valid year (within range)
-        System.out.println(year + " is leap: " + LYImproved.isLeapYear(year)); // true
-        
-        // Invalid year (outside range)
-        try {
-            LYImproved.isLeapYear(1850);
-        } catch (InvalidYearException e) {
-            System.out.println("Error: " + e.getMessage()); // Error message printed
+    
+    public static void isYearLeap(int year) throws InvalidYearException {
+        if (year < 1900 || year > 2100) {
+            throw new InvalidYearException("Neplatný rok: " + year + ". Povolený rozsah 1900-2100");
         }
+        else
+            System.out.println("No exceptions thrown");
     }
 }
--- Ukoly/Exceptions/src/com/cyrils/java/lekce14/Main.java
+++ Ukoly/Exceptions/src/com/cyrils/java/lekce14/Main.java
@@ -1,12 +1,21 @@
 package com.cyrils.java.lekce14;
 public class Main {
     //main method
-    public static void main(String[] args) throws IllegalAgeException {
-        System.out.println("1. Age Validation using IllegalAgeException");
+    public static void main(String[] args) throws IllegalAgeException, InvalidYearException {
+        System.out.println("1. Age Validation using exception IllegalAgeException");
         try {
-            CheckAge.ageValidation(17);
+            CheckAge.ageValidation(17); //throws exception
         } catch (IllegalAgeException e) {
             System.out.println("Error code: " + e.getErrorCode()); //AGE_NOT_VALID_01
+            System.out.println("Details: " + e.getDetails());
+            System.out.println("Original message: " + e.getOriginalMessage());
+        }
+        System.out.println("\n");
+        System.out.println("2. Year Validation using runtime InvalidYearException");
+        try {
+            CheckLeapYear.isYearLeap(2500); //throws exception
+        } catch (InvalidYearException e) {
+            System.out.println("Error code: " + e.getErrorCode()); //YEAR_NOT_VALID_01
             System.out.println("Details: " + e.getDetails());
             System.out.println("Original message: " + e.getOriginalMessage());
         }
