da18c9d21e9880b4684e6e9b70075f6f0d4b35ca
nightguarder <73370044+nightguarder@users.noreply.github.com>
2025-03-16 20:55:50 +0100

    create thread messenger project

--- Ukoly/Multithreading/src/com/cyrils/java/ukoly/MessageJSON.java
+++
@@ -1,8 +0,0 @@
-package com.cyrils.java.ukoly;
-
-
-//Use JSON for message serialization
-//use library like Jackson or Gson
-//https://www.baeldung.com/jackson-object-mapper-tutorial
-public class MessageJSON {
-}
